0e962b054e2000906d0066bc0bac9267553be5be
Dunbock <8299260+Dunbock@users.noreply.github.com>
2021-07-20 14:31:54 +0200

    Update Ranobes

--- src/en/Ranobes.lua
+++ src/en/Ranobes.lua
@@ -1,4 +1,4 @@
--- {"id":333,"ver":"1.0.3","libVer":"1.0.0","author":"Dunbock"}
+-- {"id":333,"ver":"1.0.4","libVer":"1.0.0","author":"Dunbock"}
 
 local baseURL = "https://www.ranobes.net"
 local settings = {}
@@ -21,7 +21,8 @@ local function convertText(textElement)
 	content = content:gsub("</p>", "")
 	content = content:gsub("<br>", "")
 	content = content:gsub("<hr>", "-------------------")
-	print(content)
+
+	return content
 end
 
 --- @param chapterURL string
