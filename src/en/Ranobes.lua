4f33475b6af1c10d8fb2a0e50b165dc0022d6410
Dunbock <8299260+Dunbock@users.noreply.github.com>
2021-08-21 19:28:35 +0200

    Update Ranobes

--- src/en/Ranobes.lua
+++ src/en/Ranobes.lua
@@ -1,4 +1,4 @@
--- {"id":333,"ver":"1.0.16","libVer":"1.0.0","author":"Dunbock"}
+-- {"id":333,"ver":"1.0.17","libVer":"1.0.0","author":"Dunbock"}
 
 local baseURL = "https://www.ranobes.net"
 
@@ -193,6 +193,7 @@ return {
 	hasCloudFlare = true,
 	hasSearch = true,
 	isSearchIncrementing = true,
+	chapterType = ChapterType.HTML,
 
 	shrinkURL = shrinkURL,
 	expandURL = expandURL,
